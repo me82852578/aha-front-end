0b84724f91184bbf0249386064df0e86fcfd4e96
Jay <me82852578@gmail.com>
2023-02-06 03:26:49 +0800

    refactor: remove sample code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
-import { usersApi } from '../../api/source';
-import { User } from '../../types';
 
 function Home() {
-  const { isSuccess, data } = useQuery({
-    queryKey: [usersApi.sourceUrl],
-    queryFn: usersApi.getData,
-  });
-
-  if (!isSuccess || !data) return <div>Loading...</div>;
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      {data.map((user:User) => (
-        <Box key={user.id} sx={{ my: 1 }}>
-          {user.name}
-        </Box>
-      ))}
-
-    </Box>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }} />
   );
 }
 
